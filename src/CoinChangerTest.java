0f312b60f841624537a3f1e0720d974f96f54073
Anda Cabrera <andacabrera29@Andas-MacBook-Pro.local>
2016-02-04 12:56:06 -0600

    Initial commit

---
+++ src/CoinChangerTest.java
@@ -0,0 +1,17 @@
+import junit.framework.TestCase;
+import java.util.ArrayList;
+
+/**
+ * Created by andacabrera29 on 2/4/16.
+ */
+public class CoinChangerTest extends TestCase {
+    private CoinChanger c;
+
+    protected void setUp() throws Exception {
+        c = new CoinChanger();
+    }
+
+    public void testCents() throws Exception {
+        assertEquals(new ArrayList<Integer>() {{ add(1);}}, c.makeChange(1));
+    }
+}
