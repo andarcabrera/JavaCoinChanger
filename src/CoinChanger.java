661b5eb3fb2ca7fd3c9f12931000ce2d0a7f5f0d
andacabrera29 <andarcabrera@gmail.com>
2016-02-04 16:56:06 -0600

    add failing test for a nickle

--- src/CoinChanger.java
+++ src/CoinChanger.java
@@ -4,9 +4,11 @@ import java.util.ArrayList;
  * Created by andacabrera29 on 2/4/16.
  */
 public class CoinChanger {
-    public ArrayList<Integer> makeChange(int i) {
+    public ArrayList<Integer> makeChange(int amount) {
         ArrayList<Integer> change = new ArrayList<>();
-        change.add(i);
+        for (int i = 0; i < amount; i += 1) {
+            change.add(1);
+        }
         return change;
     }
 }
--- src/CoinChangerTest.java
+++ src/CoinChangerTest.java
@@ -14,6 +14,9 @@ public class CoinChangerTest extends TestCase {
     public void testCents() throws Exception {
         assertEquals(new ArrayList<Integer>() {{ add(1);}}, c.makeChange(1));
         assertEquals(new ArrayList<Integer>() {{ add(1); add(1);}}, c.makeChange(2));
+        assertEquals(new ArrayList<Integer>() {{ add(1); add(1); add(1);}}, c.makeChange(3));
+        assertEquals(new ArrayList<Integer>() {{ add(1); add(1); add(1); add(1);}}, c.makeChange(4));
+        assertEquals(new ArrayList<Integer>() {{ add(5);}}, c.makeChange(5));
 
     }
 }
