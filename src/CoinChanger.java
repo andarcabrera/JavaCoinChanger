9145100e2e12c7fc281027a25a03d7bb07544c90
Anda Cabrera <andacabrera29@Andas-MacBook-Pro.local>
2016-02-04 13:26:32 -0600

    Initial commit

---
+++ src/CoinChanger.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+/**
+ * Created by andacabrera29 on 2/4/16.
+ */
+public class CoinChanger {
+    public ArrayList<Integer> makeChange(int i) {
+        ArrayList<Integer> change = new ArrayList<>();
+        change.add(i);
+        return change;
+    }
+}
